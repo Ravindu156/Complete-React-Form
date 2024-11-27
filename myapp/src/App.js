5f092483e686c11d87901e35d31d9d72c38f18ec
Ravindu Hirusanka <puresoftlk@gmail.com>
2024-11-27 11:05:54 +0000

    Initial commit

---
+++ myapp/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Form in React</h1>
+      <fieldset>
+        <form action='#' method="get">
+          <label for="firstname"></label>
+    
+
+
+
+        </form>
+      </fieldset>
+
+
+
+    </div>
+  );
+}
+
+export default App;
