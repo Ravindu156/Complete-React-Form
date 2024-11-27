57e7d265478ab890f535bd6b6ab58ab0661c104c
Ravindu Hirusanka <puresoftlk@gmail.com>
2024-11-27 13:49:40 +0000

    Add additional form fields for user input in App component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -14,18 +14,25 @@ function App() {
       <h1>Form in React</h1>
       <fieldset>
         <form action='#' method="get">
+          {/*Name */}
           <label for="firstname">First Name</label>
             <input type="text" name="firstname" id="firstname" value={firstName}/>
           
           <label for="lastname">Firt Name</label>
              <input type="text" name="lastname" id="firstname" value={lastName}/>
 
+             {/*Email */}
           <label for="email">Email</label>
              <input type="email" name="email" id="email" value={email}/>
 
+
+            {/*Contact Number */}
           <label for="contact">Contact</label>
              <input type="tel" name="email" id="email" value={contact}/>
 
+
+          
+            {/*Gender */}
           <label for="gender">Gender</label>
             <input type="radio" name="gender" value="male" id="male" checked={gender==="male"}
             onChange={}/>Male
@@ -34,9 +41,11 @@ function App() {
                   <input type="radio" name="gender" value="female" id="female" checked={gender==="female"}
             onChange={}/>Other
 
-            <label for="lang">
+            <label for="lang">  
                         Your best Subject
             </label>
+
+
 
         </form>
       </fieldset>
