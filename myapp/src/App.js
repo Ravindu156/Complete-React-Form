90b012f4941414bd45bbbb0bce083185a3ddc542
Ravindu Hirusanka <puresoftlk@gmail.com>
2024-11-27 13:47:12 +0000

    Add form fields and state management to App component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,16 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
 
 function App() {
+  const[firstName,setFirstname]=useState("");
+  const[lastName,setLastName]=useState("");
+  const[email,setEmail]=useState("");
+  const[contact,setContact]=useState("");
+
   return (
     <div className="App">
       <h1>Form in React</h1>
       <fieldset>
         <form action='#' method="get">
-          <label for="firstname"></label>
-    
+          <label for="firstname">First Name</label>
+            <input type="text" name="firstname" id="firstname" value={firstName}/>
+          
+          <label for="lastname">Firt Name</label>
+             <input type="text" name="lastname" id="firstname" value={lastName}/>
 
+          <label for="email">Email</label>
+             <input type="email" name="email" id="email" value={email}/>
 
+          <label for="contact">Contact</label>
+             <input type="tel" name="email" id="email" value={contact}/>
+
+          <label for="gender">Gender</label>
+            <input type="radio" name="gender" value="male" id="male" checked={gender==="male"}
+            onChange={}/>Male
+              <input type="radio" name="gender" value="female" id="female" checked={gender==="female"}
+            onChange={}/>Female
+                  <input type="radio" name="gender" value="female" id="female" checked={gender==="female"}
+            onChange={}/>Other
+
+            <label for="lang">
+                        Your best Subject
+            </label>
 
         </form>
       </fieldset>
